00a0cde4922e6ea2b38d37598aa57bb31940ae45
SaalimMomin <152521788+SaalimMomin@users.noreply.github.com>
2024-05-11 22:24:07 +0000

    loop

---
+++ interation/loop.js
@@ -0,0 +1,32 @@
+
+// for (let index =0; index < 10;  index++){
+//     const element = index;
+//     if (element == 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+// console.log();
+
+for (let i=1; i <=10; i++) {
+    console.log(`outer loop ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`inner loop ${j} and inner loop ${i}`);
+    }
+}
+
+let myArray = ["saalim", "momin" , "sam"]
+// console.log(myArray.length);
+for (let index = 0; index< myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+//break and continue
+for (let index = 1; index< 20; index++) {
+   if (index == 3) {
+    
+   }
+}
+
