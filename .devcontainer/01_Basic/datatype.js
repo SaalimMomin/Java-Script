fb8cd81d95ae627badcd41b3a4f826e37eb99df1
SaalimMomin <152521788+SaalimMomin@users.noreply.github.com>
2024-05-11 09:35:36 +0000

    datatype

---
+++ .devcontainer/01_Basic/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as never version
+
+// alert (3 +3) // we are using nodejs , not brower
+
+console.log("saalim");
+
+let name = "Saalim"
+let age = 23
+let isLoggedId = false
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true /false
+// null => standalone value
+//undefined =>
+// symbol = > unique
+
+// object
+
+ console.log(typeof name);
+ console.log(typeof undefined); //undefind
+
+ console.log(typeof null); // object
