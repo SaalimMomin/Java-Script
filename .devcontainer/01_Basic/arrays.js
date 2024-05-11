1af098a93a4e3e048acaeab12e7db1b3d6f44e45
SaalimMomin <152521788+SaalimMomin@users.noreply.github.com>
2024-05-11 12:51:30 +0000

    arrrays

---
+++ .devcontainer/01_Basic/arrays.js
@@ -0,0 +1,32 @@
+//array
+
+const myArr = [0, 1, 2 , 3 ,4, 5, 6]
+const myHeros = ["salman", "srk"]
+
+const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr2[2]);
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9));
+
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+
+console.log(myn1);
+console.log("c", myArr);
