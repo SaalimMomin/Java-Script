9c399cb975f6f7c3b80f4d0aaf9c6feb3791805d
SaalimMomin <152521788+SaalimMomin@users.noreply.github.com>
2024-05-11 21:51:18 +0000

    one

---
+++ control flow/one.js
@@ -0,0 +1,53 @@
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if (temperature < 50) {
+//     console.log("less than 50");
+
+// }else{
+// console.log("temperature is greter then 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
+
+// const balance = 1000
+
+// // if (balance > 500) console.log("test");
+
+
+// if (balance < 500) {
+//   console.log("less than");
+
+// }else if (balance < 750) {
+//     console.log("less than 750");
+
+// }else if (balance < 900) {
+//     console.log("less than 900");
+
+// }else{
+//     console.log("less than 1200");
+// }
+
+const isUserloggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedIbFromEmail = true
+
+if (isUserloggedIn && debitcard && 2==3) {
+    console.log("allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedIbFromEmail) {
+    console.log("user logged in");
+}
