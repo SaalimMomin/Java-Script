67837f2502ef56b44e89c85e7a68e9cb1f74a613
SaalimMomin <152521788+SaalimMomin@users.noreply.github.com>
2024-05-11 07:45:47 +0000

    container config done

---
+++ .devcontainer/01_Basic/text.js
@@ -0,0 +1,14 @@
+const accountId = 144545
+let accountEmail = "saalimmomin099@gmail.com"
+var accountPassword = "12475"
+accountCity = "islampur"
+
+// accountId = 2
+
+accountEmail = "hdb@hg.com"
+accountPassword = "1225445"
+accountCity = "kolhapur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
